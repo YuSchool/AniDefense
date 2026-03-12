3a4a274802d42bf08b9fa30875c7f8422c687505
Yusuf Sükün <yusuf.sukun94@gmail.com>
2026-03-12 22:48:12 +0100

    Phase 4 - Tower & Enemy Interaction Logic Test completed

--- Assets/Scripts/Tests/TestSpawner.cs
+++ Assets/Scripts/Tests/TestSpawner.cs
@@ -2,19 +2,26 @@ using UnityEngine;
 
 public class TestSpawner : MonoBehaviour
 {
-    [Header("Test Einstellungen")]
+    [Header("Gegner")]
     [SerializeField] private GameObject gegnerPrefab;
     [SerializeField] private Transform[] route;
 
+    [Header("Tower")]
+    [SerializeField] private GameObject towerPrefab;
+    [SerializeField] private Transform towerPosition;
+
     private void Start()
     {
+        // Gegner spawnen
         GameObject gegner = Instantiate(
             gegnerPrefab,
             route[0].position,
             Quaternion.identity
         );
-
         PathFollower pf = gegner.GetComponent<PathFollower>();
         pf.Initialisiere(route, 3f);
+
+        // Tower spawnen
+        Instantiate(towerPrefab, towerPosition.position, Quaternion.identity);
     }
 }
